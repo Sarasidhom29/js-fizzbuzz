50285cc7b73a8e08db04845ca5f9e91dbd91cac0
Sara Sidhom <s.sara.29.07.2006@gmail.com>
2026-03-25 20:54:02 +0100

    collegato documento .js e svolta prima stesura dell'esercizio

--- script.js
+++ script.js
@@ -5,3 +5,24 @@
 //per i multipli di 5 stampi Buzz.
 //per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
+for(let i = 0; i < 100; i++) {
+
+    //utilizzata una doppia condizione con modale
+    //(simbolo percentuale -> divisione per n, ma stampa il resto)
+    if(i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz');
+
+    //se condizione vera stampa Fizz
+    } else if (i % 3 === 0) {
+        console.log('Fizz');
+    
+    //se condizione vera stampa Buzz
+    } else if (i % 5 === 0) {
+        console.log('Buzz');
+
+    //se condizione vera stampa stringa alternativa
+    } else {
+        console.log('Non è multiplo ne di 3 ne di 5')
+    }
+    
+}
